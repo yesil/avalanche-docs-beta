43d9d80c6f11653e3e167aba5e7593b562cef841
Yulin Dong <yulin.dong@avalabs.org>
2021-11-08 10:46:09 -0700

    add avax logo/favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.png',
   organizationName: 'Ava Labs', // Usually your GitHub org/user name.
   projectName: 'Developer Docs', // Usually your repo name.
 
