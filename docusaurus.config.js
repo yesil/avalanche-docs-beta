783cd7529b34890d5f76863413d64b0407400caa
Yulin Dong <yulin.dong@avalabs.org>
2021-11-07 20:22:13 -0700

    remove reference error to intro and blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -56,15 +56,6 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
             title: 'Community',
             items: [
               {
@@ -84,10 +75,6 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
               {
                 label: 'GitHub',
                 href: 'https://github.com/facebook/docusaurus',
