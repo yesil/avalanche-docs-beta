53cca776ad03d9071e063f576742c3277052408b
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2021-11-11 16:34:39 -0500

    nits

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Avalanche',
-  tagline: 'Avalanche Docs',
+  title: 'Avalanche Docs',
+  tagline: 'Documentation and guides for Avalanche',
   url: 'https://docs-beta.avax.network',
   baseUrl: '/',
   onBrokenLinks: 'throw',
