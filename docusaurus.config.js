1dd2d622a78ce171006393470b9ce121b3986dd4
Yulin Dong <yulin.dong@avalabs.org>
2021-11-11 19:19:54 -0700

    change over to avalanche-docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.png',
   organizationName: 'ava-labs', // Usually your GitHub org/user name.
-  projectName: 'avalanche-docs-beta', // Usually your repo name.
+  projectName: 'avalanche-docs', // Usually your repo name.
 
   plugins: [require.resolve('docusaurus-lunr-search')],
 
@@ -32,7 +32,7 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           routeBasePath: '/',
           // Please change this to your repo.
-          editUrl: 'https://github.com/ava-labs/avalanche-docs-beta/edit/main/',
+          editUrl: 'https://github.com/ava-labs/avalanche-docs/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -52,7 +52,7 @@ const config = {
         },
         items: [
           {
-            href: 'https://github.com/ava-labs/avalanche-docs-beta',
+            href: 'https://github.com/ava-labs/avalanche-docs',
             label: 'GitHub',
             position: 'right',
           },
